faa4d9d00c6b3239c234480c35025af893522db6
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-08-02 03:07:12 +0700

    fix: fix path error

--- src/utils/php-generator.ts
+++ src/utils/php-generator.ts
@@ -4,9 +4,16 @@ import stringCase from './string-case'
 
 export default {
   generate(type: string, name: string): void {
+    // load text from templates
     const fileText = fs.readFileSync(path.join(__dirname, `../templates/${type}.txt`), 'utf-8')
+
+    // create file
     fs.writeFileSync(
-      path.join(__dirname, `/app/${type}/${name}${stringCase.toTitleCase(type)}.php`),
+      path.join(process.cwd(), `/app/${type}/${name}${stringCase.toTitleCase(type)}.php`),
       fileText.replace(/###/g, name).replace(/##/, `/${name.toLowerCase()}`))
+
+    // append dependency to requirements.php
+    fs.appendFileSync(path.join(process.cwd(), '/requirements.php'),
+      `require_once(__DIR__ . '/app/${type}/${name}${stringCase.toTitleCase(type)}.php');`)
   },
 }
