373154097f974438d471ef877b8c900bccc719c3
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-08-02 17:47:51 +0700

    feat: add model command

---
+++ src/commands/model.ts
@@ -0,0 +1,30 @@
+import {Command, flags} from '@oclif/command'
+import stringCase from '../utils/string-case'
+import phpGenerator from '../utils/php-generator'
+import * as chalk from 'chalk'
+import cli from 'cli-ux'
+
+export default class Middleware extends Command {
+  static description = 'create a RESTup Model'
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+  }
+
+  static args = [
+    {
+      name: 'name',
+      required: true,
+      description: 'name of the model',
+      parse: (input: string) => stringCase.toTitleCase(input),
+    },
+  ]
+
+  async run() {
+    const {args} = this.parse(Middleware)
+
+    cli.action.start(`${chalk.blue('Creating')} ${chalk.yellow(`${args.name}`)} for you`)
+    await phpGenerator.generate('model', args.name)
+    cli.action.stop(chalk.green('\nDone.'))
+  }
+}
